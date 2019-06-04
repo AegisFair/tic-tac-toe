1b2ae22aee4ad4d884c8174bb54d9d96808e14d6
AegisFair <geforcee1995@gmail.com>
2019-06-04 20:10:26 +0300

    create method which view grid with the given size, add base style for game grid

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.onload=init;
 function init(){
     // onload
-
+    var gamesTicTac=new Tic_tac();
+    gamesTicTac.viewGamingField(3);
     // click
 
 }
--- tic_tac.js
+++ tic_tac.js
@@ -1,20 +1,29 @@
-
-// Начальная инициализация
-    // получение размера сетки
-var gamesTicTac=(function(){
-    var gridSize=prompt("Размер игрового поля, введите одно число (min=3): ",3);
-    if (!isNaN(parseFloat(gridSize) && isFinite(gridSize)) && gridSize>=3) {
-        return new Tic_tac(gridSize);
-    }else{
-        alert("Испугались?=)")
+function Tic_tac() {
+    //Ход игрока 1|2
+    this.turn=1;
+    // Создание игрового поля
+    this.viewGamingField=function (gridSize) {
+        var wrapper=document.createElement('div'),
+        table=document.createElement('table');
+        wrapper.className="wrapperGrid";
+        table.className="gameGrid";
+        for (let i = 0; i < gridSize; i++) {
+            var row=document.createElement('tr');
+            for (let j = 0; j < gridSize; j++) {
+                var td=document.createElement('td');
+                td.onclick=clickCell;
+                row.appendChild(document.createElement('td'));
+            }
+            table.appendChild(row);
+        }
+        document.querySelector("body").appendChild(wrapper).appendChild(table);
+    };
+    function clickCell(cell){
+        // Поле пустое
+        // if(cell.target.innerHTML)
+        console.log(cell.target.innerHTML);
     }
-})();
-console.log(gamesTicTac);
-// Конструктор
-// передать размер поля
-function Tic_tac(gridSize) {
-    // Инициализация сетки
-    this.gridSize=gridSize;
-    // Отображение сетки
+    function checkCellEmpty(cell){
 
+    }
 }
